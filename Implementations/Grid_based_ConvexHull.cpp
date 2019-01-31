2673e7fbf57ae41dee7201403ede2c848733ba64
Ajwad <shaikhajwad10@gmail.com>
2019-02-01 03:34:08 +0530

    Programmed to take input points

--- Implementations/Grid_based_ConvexHull.cpp
+++ Implementations/Grid_based_ConvexHull.cpp
@@ -10,5 +10,13 @@ using namespace std;
 
 int main()
 {
-
+    vector < pair <double, double > > P; //Dynamic Array of Points P
+    int setSize; // Size of the set of points to be given
+    cin >> setSize;
+    for(int a  = 0; a < setSize; a++)
+    {
+        double x, y;
+        cin >> x >> y;
+        P.push_back(make_pair(x, y)); // Adding points to the Dynamic Array P
+    }
 }
