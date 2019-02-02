261062496e4f082159cb51f9920a779ec7eb9ac8
Ajwad <shaikhajwad10@gmail.com>
2019-02-03 01:31:53 +0530

    Defining Boundaries for Input Points

--- Implementations/Grid_based_ConvexHull.cpp
+++ Implementations/Grid_based_ConvexHull.cpp
@@ -10,13 +10,28 @@ using namespace std;
 
 int main()
 {
-    vector < pair <double, double > > P; //Dynamic Array of Points P
+    vector < pair <double, double > > S; //Dynamic Array of Points S
     int setSize; // Size of the set of points to be given
     cin >> setSize;
+
     for(int a  = 0; a < setSize; a++)
     {
         double x, y;
         cin >> x >> y;
-        P.push_back(make_pair(x, y)); // Adding points to the Dynamic Array P
+        S.push_back(make_pair(x, y)); // Adding points to the Dynamic Array S
     }
+
+    // We build a square grid so with equal number of rows and columns viz. R = C = sq. root ( setSize )
+    int gridSide = sqrt(setSize);
+    double topBoundary = (double) INT_MIN, endBoundary = (double) INT_MIN;
+    double bottomBoundary = (double) INT_MAX, startBoundary = (double) INT_MAX;
+
+    for(int a = 0; a < S.size(); a++)
+    {
+    	startBoundary = min(startBoundary, S[a].first);
+    	endBoundary = max(endBoundary, S[a].first);
+    	bottomBoundary = min(bottomBoundary, S[a].second);
+    	topBoundary = max(topBoundary, S[a].second);
+    }
+
 }
